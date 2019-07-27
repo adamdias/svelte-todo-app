2bd4be695105c8cba146d40436ab284eca20fdde
Adam Dias <adam.dias@outlook.com>
2019-07-27 18:20:40 -0300

    Update app

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,20 @@
 <script>
-	export let name;
+ import Tasks from "./Tasks/Task.svelte";
+ const projectName = "To Do App";
+ const src="https://avatars2.githubusercontent.com/u/23617963?s=40&v=4";
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
-
-<h1>Hello {name}!</h1>
+<section class="section">
+ <div class="container">
+   <figure class="image is-64x64">
+     <img {src} alt="Svelte3"/>
+   </figure>
+   <!-- dentro de chaves {} podemos usar javascript -->
+   <h1 class="title">{projectName.toUpperCase()}</h1>
+   <p class="subtitle">
+     My first website made with
+     <strong>Svelte!</strong>
+   </p>
+ </div>
+	<Tasks />
+</section>
---
+++ src/Tasks/TaskGridItem.svelte
@@ -0,0 +1,17 @@
+<script>
+    export let id; 
+    export let description;
+    
+    import { createEventDispatcher } from "svelte";
+    const dispatch = createEventDispatcher();
+</script>
+
+<tr>
+    <td>{description}</td>
+    <td on:click={() => dispatch('edit', { id: id, description: description })}>
+       <i class="fas fa-edit" />
+     </td>
+    <td on:click={() => dispatch('delete', { id: id })}>
+       <i class="fas fa-trash" />
+     </td>
+</tr>
